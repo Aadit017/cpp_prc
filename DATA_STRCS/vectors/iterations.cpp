0ed299936b637728d9e455f4e6a2e5e4a60f6276
Aadit <baggaaadit017@gmail.com>
2024-03-27 23:12:23 +0530

    bde

--- DATA_STRCS/vectors/iterations.cpp
+++ DATA_STRCS/vectors/iterations.cpp
@@ -20,4 +20,16 @@ int main()
     {
         cout << *i << " ";
     }
-    cout
+    cout << "\nin reverse now" << endl;
+    for (auto i = v1.rbegin(); i != v1.rend(); i += 1)
+    {
+        cout << *i << " ";
+    }
+    // capacity functions now
+    cout << "\nsize of the vector [number of elements]" << endl;
+    cout << v1.size() << endl;
+    cout << "Maximum number of elements the vector can hold " << v1.max_size() << endl;
+    cout << "Storage associated to the vector depending upon the number of elements in it " << v1.capacity() << endl;
+    // to resize and the shrink the elemnets to that size we have two functions
+    cout << "vector empty ? " << v1.empty() << endl;
+}
